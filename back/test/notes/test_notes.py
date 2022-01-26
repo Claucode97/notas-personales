d8faab1d4224748be82285f6e221b877c31dacf4
vicky <v.pinero70@gmail.com>
2022-01-26 11:02:22 +0100

    src

---
+++ back/test/notes/test_notes.py
@@ -0,0 +1,13 @@
+from src.lib.utils import temp_file
+
+from src.webserver import create_app
+from src.domain.info_note import NotesRepository, Note
+
+def test_should_return_empty_list_of_notes():
+    notes_repository = NotesRepository(temp_file())
+    app = create_app(repositories={"note": notes_repository})
+    client = app.test_client()
+
+    response = client.get("/api/my-notes")
+
+    assert response.json == []
