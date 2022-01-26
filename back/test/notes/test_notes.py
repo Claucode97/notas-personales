feb03d9759837eda9d1090d7d5821729c466f4cd
rocio <rocio.gabriela.03c@gmail.com>
2022-01-26 13:04:33 +0100

    test list of notes

--- back/test/notes/test_notes.py
+++ back/test/notes/test_notes.py
@@ -11,3 +11,20 @@ def test_should_return_empty_list_of_notes():
     response = client.get("/api/my-notes")
 
     assert response.json == []
+
+
+def test_should_return_list_of_notes():
+    notes_repository = NotesRepository(temp_file())
+    app = create_app(repositories={"note": notes_repository})
+    client = app.test_client()
+    note = Note(title="example1", text="text example")
+    notes_repository.save(note)
+    response = client.get("/api/my-notes")
+
+    assert response.json == [
+        {
+            "title": "example1",
+            "text": "text example"
+        }
+
+    ]
