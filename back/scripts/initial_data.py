f72f39efb6360ebfb70a291bc2b167c2dd4f7037
Laurafarrerdev <laurafarrer1@gmail.com>
2022-01-28 12:10:12 +0100

    Se crea la BD y las tablas desde el initial_data.py sin errores (acceso correcto a los datos)

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -11,15 +11,15 @@ def main():
     info_repository = InfoRepository(database_path)
 
     info_repository.save(Info(app_name="f5-my-notes-app2"))
-    print("creando obj")
-
-    nota1_compras = Note(title="Lista de la compra:", text="Pan y Chorizo")
-    nota2 = Note(title='Bebidas', text='Vino y agua')
-    print("obj creado")
 
     notes_repository = NotesRepository(database_path)
     print("repositorio notes hecho")
-    notes_repository.save(nota1_compras)
+    notes_repository.save(
+        Note(title="Lista de la compra:", text="Pan y Chorizo"))
+
+    print("creando obj")
+    nota2 = Note(title='Bebidas', text='Vino y agua')
+    print("obj creado")
     notes_repository.save(nota2)
 
 
