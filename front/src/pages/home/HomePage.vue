35d094fdd231985974ef7389dc7624d195773855
vicky <v.pinero70@gmail.com>
2022-03-01 14:02:30 +0100

    tarea #110 home page

--- front/src/pages/home/HomePage.vue
+++ front/src/pages/home/HomePage.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home"> 
     <h1>{{ personalnotes }}</h1>
-    <p> my notes</p>
+    <section>
+      <select v-model="selectedUser">
+        <option :value="null" disable>Select User</option>
+        <option v-for="user in users" :value="user" :key="user.id">
+            {{ user.name }}
+          </option>
+      </select>
+    </section>
+    <button @click="onButtonClicked"> Ver mis notas</button>
+    
   </div>
 </template>
 
@@ -11,10 +20,37 @@ export default {
   name: 'Home',
   data() {
     return {
-    personalnotes:"WELCOME"
+    personalnotes:"WELCOME",
+    users:[],
+    selectedUsers: null,
+    };
+  },
+
+  mounted(){
+    this.loadData();
+  },
+methods: {
+  async loadData() {
+    this.users = [
+      {
+        id: "user-1",
+        name:"Pepa",
+      },
+      {
+        id:"user-2",
+        name:"Pepe",
+      },
+    ];
+  },
+
+  onButtonClicked(){
+    localStorage.userId = this.selectedUser.id;
+    localStorage.userName = this.selectedUser.name;
+    this.$root.$forceUpdate();
+    this.$router.push ("/notes")
     }
   }
-}
+};
 </script>
 
 <style scoped>
