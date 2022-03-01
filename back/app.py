dcabafe6f01bc9e60eb16eb9acfc8e7e42f67dd2
rocio <rocio.gabriela.03c@gmail.com>
2022-03-01 14:20:35 +0100

    import user

--- back/app.py
+++ back/app.py
@@ -2,6 +2,7 @@ import sqlite3
 from src.webserver import create_app
 from src.domain.note import NotesRepository
 from src.domain.info import InfoRepository
+from src.domain.user import UserRepository
 
 
 database_path = "data/database.db"
